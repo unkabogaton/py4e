70d212da5e763721ae91f1aea2e919b8b40ea6a5
unkabogaton <anthonralph2@gmail.com>
2020-03-04 16:32:06 +0800

    Comment

--- Mar3Function/ex1.py
+++ Mar3Function/ex1.py
@@ -2,11 +2,10 @@
 hours=input("Enter Hours: ")
 rate=input("Enter Rate: ")
 def computepay(hours, rate):
-
-if hours > 40:
-    pay=(hours-40)*1.5*rate+40*rate
-else:
-    pay=hours*rate
-return pay
-x = computepay(hours, rate)
-print("Your pay is ") + str(x)
+    if hours > 40:
+        pay=(hours-40)*1.5*rate+40*rate
+    else:
+        pay=hours*rate
+    return pay
+x=computepay(hours,rate)
+print("Your pay is " + str(x))
