5ee4e5fa6c600b40498aa477e13e77b7b086f93f
unkabogaton <anthonralph2@gmail.com>
2020-03-04 17:05:58 +0800

    Yes Natapos din

--- Mar3Iterations/ex1.py
+++ Mar3Iterations/ex1.py
@@ -4,11 +4,14 @@ count=0
 while True:
     try:
         n=input("Enter a number: ")
-        if (n=="done"): 
+        if (n=="done"):
             break
         n=int(n)
         sum=sum+n
         count=count+1
     except:
         print("Invalid input")
-print (sum, count, sum/count)
+
+print ("Sum: " + str(sum))
+print ("Count: " + str(count))
+print ("Average: " + str(sum/count))
