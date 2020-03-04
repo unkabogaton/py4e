36751192c13988a77e036149646b4358abb258cc
unkabogaton <anthonralph2@gmail.com>
2020-03-04 15:48:30 +0800

    Comment

--- Mar3Function/ex1.py
+++ Mar3Function/ex1.py
@@ -1,17 +1,12 @@
 
 hours=input("Enter Hours: ")
 rate=input("Enter Rate: ")
+def computepay(hours, rate):
 
-if int(hours) <= 40:
-  def computepay(hours, rate):
-    wages = int(hours) * int(rate)
-    return wages
-  x = computepay(hours, rate)
-  print("Pay: ") + str(x)
+if hours > 40:
+    pay=(hours-40)*1.5*rate+40*rate
 else:
-  overhours = int(hours) - 40
-  def computepay(overhours, rate):
-    wages = (int(overhours) * int(rate) * 1.5) + (40 * int(rate))
-    return wages
-  x = computepay(overhours, rate)
-  print("Pay: ") + str(x)
+    pay=hours*rate
+return pay
+x = computepay(hours, rate)
+print("Your pay is ") + str(x)
