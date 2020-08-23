ab303464f460ecc4ae80c01c872dfe8413bbdb37
unkabogaton <anthonralph2@gmail.com>
2020-08-23 12:22:23 +0800

    jj

---
+++ py4e.py
@@ -0,0 +1,12 @@
+big = max('hELlo World')
+print (big)
+>>w -because w is the highest number based on the order of alpahabet
+
+tiny= min('hello world')
+>>   (space between hell and world- because the space is the lowest in alphabet
+
+i=42
+type(i)
+>> <class 'int'>  - type is use to know the type of data stored in a variabe, 
+     it is either integer 'int' or string 'str'
+     
